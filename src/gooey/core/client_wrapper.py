e7528e864121004e5a5713f8e2bdd733c394eb02
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-09-11 09:44:14 +0000

    Release 0.0.1-beta8

--- src/gooey/core/client_wrapper.py
+++ src/gooey/core/client_wrapper.py
@@ -22,7 +22,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "gooeyai",
-            "X-Fern-SDK-Version": "0.0.1-beta7",
+            "X-Fern-SDK-Version": "0.0.1-beta8",
         }
         headers["Authorization"] = f"Bearer {self._get_api_key()}"
         return headers
